13fc773d37488fc5726e76b8cbe8ba08d425e8c5
Mukhiddin <sirojiddinovm720@mail.ru>
2024-02-12 18:19:05 +0500

    add phone number to user by auto

--- app/models.py
+++ app/models.py
@@ -22,6 +22,7 @@ class User(Base):
     id = Column(Integer, primary_key= True, nullable= False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable= False)
+    phone_number = Column(String, nullable= True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
 class Vote(Base):
